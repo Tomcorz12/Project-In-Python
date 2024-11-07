4def9a25cd027a292d8876ef7866d7a5b505a835
Tomas Gonzalez <tomcorz12@gmail.com>
2024-11-06 20:41:53 -0500

    Added a new folder for more exercises

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 cd game
 python3 game.py
 ```
+
+# Steps for App Folder
+Completar los demás archivos que faltan dentro del programa para poder tener una mejor ejecución y entendimiento del programa
+que vamos a estar completando dentro de este curso, entonces tener listos esos programas para el jueves EOD para poder continuar
---
+++ app/charts.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+def generateBarChart(labels, values):
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    plt.savefig('bar.png')
+    plt.close()
+    
+    
+def generatePieChart(labels, values):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels = labels)
+    plt.savefig('pie.png')
+    plt.close()
+    
+if __name__ == '__main__':
+    labels = ['A', 'B', 'C']
+    values = [10, 40, 800]
+    generateBarChart(labels, values)
+    generatePieChart(labels, values)
